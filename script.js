36644c80ec12a85b569740cefb0526bf9f1f68ae
ArtXenos <aguadalquiver_250000001095@uic.edu.ph>
2026-08-10 21:23:50 +0800

    Update

--- script.js
+++ script.js
@@ -41,8 +41,7 @@ sections.forEach(section => observer.observe(section));
                 { src: "Portfolio/UFY1.png"},
                 { src: "Portfolio/UFY2.jpg"},
                 { src: "Portfolio/UFY3.jpg"},
-                { src: "Portfolio/UFY4.jpg"},
-                { src: "Portfolio/UFY5.jpg"}
+                { src: "Portfolio/UFY4.png"},
             ]
         },
         honors: {
